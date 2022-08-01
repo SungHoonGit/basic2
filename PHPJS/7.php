86a88f0757e17f0bccd74e7318d98b912dc443dd
Sunghoon Kim <ksa134652@gmail.com>
2022-08-01 23:50:49 +0900

    making_id

---
+++ PHPJS/7.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+    <script>
+      password = prompt("비밀번호");     // 비밀번호 입력 메소드
+       //  html, css 는 할수 없는 일을 js, php 는 가능
+      if(password == 1111) {
+        document.write("안녕하세요. 주인님");
+      } else {
+        document.write("뉘신지?");
+      }
+    </script>
+</body>
+</html>
